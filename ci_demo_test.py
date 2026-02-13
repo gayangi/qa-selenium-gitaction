e2bf874cd9330240e71c79682196724fd77a5a67
gayangi <gayangidanangika1@gmail.com>
2026-02-13 11:29:31 +0100

    Update ci_demo_test.py

--- ci_demo_test.py
+++ ci_demo_test.py
@@ -1,2 +1,7 @@
-def test_ci_demo():
-    assert 1 + 1 == 2
+# def test_ci_demo():
+#     assert 1 + 1 == 2
+
+test_password_minimum_length():
+    password = "Qa1234"
+
+    assert len(password) >= 6
